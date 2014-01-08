652cc590da39514b0ddb7c3cae1fd81b04aebe0d
Maik Hertha <maik.hertha@volkswagen.de>
2014-01-08 11:31:10 +0100

    GNU Lizenz hinzugefügt.

--- setup-emacs-registry.rb
+++ setup-emacs-registry.rb
@@ -1,5 +1,24 @@
 #! ruby -w
 # -*- coding: iso-8859-1 -*-
+#
+# setup-emacs-registry.rb
+# Create windows registry definitions to integrate GNU emacs within
+# the windows environment.
+#
+# Copyright (C) 2014 Maik Hertha (mhertha@gmail.com)
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 require 'optparse'
 
@@ -498,6 +517,15 @@ options = {
 
 opt_parser = OptionParser.new do |opts|
   opts.on('-h', '--help', 'Display this help') do
+    puts <<-EOL
+setup-emacs-registry.rb
+Copyright (C) 2014 Maik Hertha (mhertha@gmail.com)
+    
+This program comes with ABSOLUTELY NO WARRANTY.
+This is free software, and you are welcome to redistribute it
+under certain conditions.
+
+EOL
     puts opts
     exit
   end
